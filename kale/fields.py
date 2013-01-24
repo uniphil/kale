b866c62a35f24a4df69c6f4deef1b6f3a380b506
Philip Schleihauf <uniphil@gmail.com>
2013-01-24 17:11:49 -0500

    sensibility

--- kale/fields.py
+++ kale/fields.py
@@ -11,30 +11,23 @@ f == Field(Field(Field(Field(f.raw).raw).raw).raw)
 
 """
 
-from abc import ABCMeta, abstractproperty
+from abc import ABCMeta, abstractmethod, abstractproperty
 from types import UnicodeType
 
 class BaseField(object):
     """inheriting from this might keep you in line."""
     __metaclass__ = ABCMeta
-
-    @abstractproperty
+    @abstractmethod
     def raw(self): return # yum
 
 
 class SelfRaw(BaseField):
-    """for types that already represent their state completely
-    simply adds a simple raw property"""
-    @property
+    """for types that already represent and initialize their state completely
+    with repr -- simply wraps repr with the required raw property"""
     def raw(self):
         return repr(self)
 
         
-"""
-yes these are kind of dumb.
-but it feels good to import library classes, right?
-"""
-
 #class Boolean(SelfRaw, bool): pass # lol nope
 class Integer(SelfRaw, int): pass # DANGER: watch for overflows...
 class Float(SelfRaw, float): pass
@@ -45,51 +38,35 @@ class Tuple(SelfRaw, tuple): pass
 class List(SelfRaw, list): pass
 class Dictionary(SelfRaw, dict): pass
 
+class String(object):
+    """docstring for String"""
+    def __init__(self, arg):
+        super(String, self).__init__()
+        self.arg = arg
 
-class AttrState(BaseField):
-    """For fields whose state is tracked as simple-valued attributes"""
+
+
+class SimpleState(BaseField):
+    """fields whose state is tracked by simple properties."""
+    __metaclass__ = ABCMeta
+
     def __init__(self, state):
         for name, value in state.items():
             setattr(self, name, value)
-        self._acceptable_types = [int, float, complex, str, tuple, list, dict]
-        self._exclude_attributes = ['_acceptable_types', '_exclude_attributes']
 
-    @property
+    @abstractproperty
+    def _state_property_names(self):
+        """a list of property names on the object"""
+        return ()
+    
     def raw(self):
-        return dict((name, value) for name, value in self.__dict__.items()
-            if type(value) in self._acceptable_types and
-            name not in self._exclude_attributes)
+        state = dict((n, getattr(self, n)) for n in self._state_property_names)
+        return state
 
 
-class Periodic(AttrState):
+class Periodic(SimpleState):
     """when does it happen next?"""
+    _state_property_names = ('period', 'phase')
+
     def next(self):
-        return self.last + 'blahblah'
-        
-p = Periodic({'last': 'tuesday', 'abc': 'wednesday'})
-
-print p.last
-print p.raw
-
-q = Periodic(p.raw)
-
-print q.next()
-print q.raw
-
-if __name__ == '__main__':
-
-    class GoodF(BaseField):
-        @property
-        def value(self): return 5
-        @property
-        def raw(self): return 'a'
-    f1 = GoodF()
-
-    class SomeF(BaseField): pass
-
-    try:
-        f2 = SomeF()
-    except TypeError:
-        pass
-    else:
-        raise Exception('you shouldn\'t be allowed')
+        return self.last + 'blahhh'
